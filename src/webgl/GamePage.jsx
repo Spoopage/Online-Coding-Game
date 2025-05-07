f411bd4e71be4a7330519bbeb429ba70f4824277
Spoopage <adrian.husodo82@gmail.com>
2025-05-07 17:42:16 +0700

    testing a tester game with local storage function

--- src/webgl/GamePage.jsx
+++ src/webgl/GamePage.jsx
@@ -3,22 +3,30 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 function GamePage() {
     const { unityProvider } = useUnityContext({
-        loaderUrl: "build/webgl/tester_game.loader.js",
-        dataUrl: "build/webgl/tester_game.data.unityweb",
-        frameworkUrl: "build/webgl/tester_game.framework.js.unityweb",
-        codeUrl: "build/webgl/tester_game.wasm.unityweb",
+        loaderUrl: "database.",
+        dataUrl: "build/webgl/Build.data.unityweb",
+        frameworkUrl: "build/webgl/Build.framework.js.unityweb",
+        codeUrl: "build/webgl/Build.wasm.unityweb",
     });
-    
+
     return (
-        <>
-            <div>
-                <h1>This is game page </h1>
+        <div style={{ width: "100vw", height: "100vh", backgroundColor: "#fff" }}>
+            <h1 style={{ textAlign: "center", marginTop: "1rem" }}>This is game page</h1>
+
+            <div
+                style={{
+                    width: 1280,
+                    height: 720,
+                    position: "fixed",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    backgroundColor: "#000", // optional, biar kontras
+                }}
+            >
+                <Unity unityProvider={unityProvider} style={{ width: "100%", height: "100%" }} />
             </div>
-            <div>
-                <Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
-            </div>
-        </>
+        </div>
     );
 }
-
 export default GamePage;
