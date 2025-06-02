42145aed21fe4f74f7114b6710bb3165dfc24ab1
Spoopage <adrian.husodo82@gmail.com>
2025-06-02 16:27:54 +0700

    feat:added supabase connection

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,52 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import GamePage from "./webgl/GamePage";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import SubmitGamePage from "./pages/SubmitGamePage";
 import Navbar from "./components/Navbar";
+import { supabase } from './supabaseClient';
 
 function App() {
   const [profile, setProfile] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [dataFetched, setDataFetched] = useState(false);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/user")
-      .then((res) => {
-        if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
-        return res.json();
-      })
-      .then((data) => {
-        setProfile(data);
-      })
-      .catch((err) => console.error("Fetch error:", err))
-      .finally(() => setIsLoading(false));
-  }, []);
+  // // Local DB
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/user")
+  //     .then((res) => {
+  //       if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       setProfile(data);
+  //     })
+  //     .catch((err) => console.error("Fetch error:", err))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
 
   useEffect(() => {
     if (profile.length > 0) {
       setDataFetched(true);
     }
   }, [profile]);
+
+  // Supabase
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from('games').select('*');
+      if (error) {
+        console.error('Error fetching data:', error);
+      } else {
+        console.log('Data:', data);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Router>
